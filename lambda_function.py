88323300574e36245a7eadfbb5acc422cc94a55d
soumoks <soumoks@gmail.com>
2020-05-08 19:31:07 -0600

    updated cloudformation template

--- lambda_function.py
+++ lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hi Pablo!')
+        'body': json.dumps('Hello from Lambda!')
     }
