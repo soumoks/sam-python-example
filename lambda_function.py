bd8fccb38c027d7feb9df48ecf5b1079c5df80d5
soumoks <soumoks@gmail.com>
2020-05-11 16:05:48 -0600

    Triggering pipeline

--- lambda_function.py
+++ lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Testing CI/CD!')
+        'body': json.dumps('Hi Camilo!')
     }
